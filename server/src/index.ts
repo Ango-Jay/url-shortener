8aae0d6e19b00d20ae85582b85dc5def7b732ca7
Ango Jeffrey <angojeff433@gmail.com>
2026-08-16 18:48:57 +0100

    Add cors and body parser middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,16 @@
 import Koa from "koa";
+import cors from "@koa/cors";
+import bodyParser from "koa-bodyparser";
 import { healthModule } from "./modules/health";
 
 const app = new Koa();
 const port = Number(process.env.PORT) || 3000;
 
+// middleware
+app.use(cors());
+app.use(bodyParser());
+
+// modules
 healthModule.install(app);
 
 app.listen(port, () => {
